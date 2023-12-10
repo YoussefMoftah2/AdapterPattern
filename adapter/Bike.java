cf8ab0e00234891356bc5e17279949be74d4d5b7
YoussefMoftah2 <112041838+YoussefMoftah2@users.noreply.github.com>
2023-12-10 11:53:49 +0200

    Add files via upload

---
+++ adapter/Bike.java
@@ -0,0 +1,14 @@
+package adapter;
+
+public class Bike {
+	public void padal () {
+		System.err.println("bike start moving");
+	}
+public void pushbreak () {
+	System.out.println("bike stopped");
+}
+	public void ringbell() {
+		System.out.println("ring ring ..");
+	}
+
+}
---
+++ adapter/Vichle.java
@@ -0,0 +1,9 @@
+package adapter;
+
+public interface Vichle {
+	void accelrate() ;
+	void pushbreak () ;
+	void soundhorn() ;
+	
+
+}
